cabb97fc14030fde5eea948010690f4fdf4d6729
Masana Pam <mahubed9539@gmail.com>
2023-06-23 14:02:36 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
  const StoreHistory=[];
- 
+ const historyElem=document.getElementById('history');
 
 const element = document.getElementById("btn");element.addEventListener("click",
 function (){
+
     let txt = document.getElementById('input-text').value;
     checkPalindrome(txt);
 });
@@ -13,20 +14,24 @@ function checkPalindrome(txt){
     let halfLen = Math.floor(len/2);
     let display = document.getElementById("display");
     let i;
-
+    let status="";
     for(i=0; i < halfLen; i++){
         if(txt_new[i] !== txt_new[len-1-i]){
+            status="not a palendrome";
          display.textContent="No! its not a palindrome";
         }else{
-        display.textContent="Yes! its a palindrome";}
+            status="its a palindrome";
+            display.textContent="Yes! its a palindrome";}
     }
     
-    StoreHistory.push(txt_new);
+    StoreHistory.push(`<li>${txt_new} : ${status}</li>`);
     console.log(StoreHistory);
-
+    let displayHistory="";
+    for(let i of StoreHistory) {
+        displayHistory+=i
+    }
+    historyElem.innerHTML=displayHistory;
     return;
-}
-
-;
+};
 
 
