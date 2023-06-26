f0e0b1c2d7a3183c3c5296a8f6ca5e86add87382
Masana Pam <mahubed9539@gmail.com>
2023-06-26 11:24:54 +0200

    update

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
- const StoreHistory=[];
- const historyElem=document.getElementById('history');
+const StoreHistory = [];
+const historyElem = document.getElementById('history');
 
-const element = document.getElementById("btn");element.addEventListener("click",
-function (){
+const element = document.getElementById("btn"); element.addEventListener("click",
+    function () {
 
-    let txt = document.getElementById('input-text').value;
-    checkPalindrome(txt);
-});
+        let txt = document.getElementById('input-text').value;
+        checkPalindrome(txt);
+    });
 
-function checkPalindrome(txt){
-    let txt_new = txt.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
-    let len=txt_new.length;
-    let halfLen = Math.floor(len/2);
+function checkPalindrome(txt) {
+
+
+    let txt_new = txt.replace(/[\W_]/g, '').toLowerCase();
+    let len = txt_new.length;
+     let halfLen = Math.floor(len/2);
     let display = document.getElementById("display");
     let i;
-    let status="";
+    let status = "";
+
     for(i=0; i < halfLen; i++){
         if(txt_new[i] !== txt_new[len-1-i]){
-            status="not a palendrome";
-         display.textContent="No! its not a palindrome";
-        }else{
-            status="its a palindrome";
+            status="Not a palindrome:false";
+            display.textContent="No! its not a palindrome";
+        }else if(txt_new[i]=== txt_new[len-1]) {
+            status="its a Palindrome:true";
             display.textContent="Yes! its a palindrome";}
     }
-    
+
     StoreHistory.push(`<li>${txt_new} : ${status}</li>`);
     console.log(StoreHistory);
-    let displayHistory="";
-    for(let i of StoreHistory) {
-        displayHistory+=i
+    let displayHistory = "";
+    for (let i of StoreHistory) {
+        displayHistory += i
     }
-    historyElem.innerHTML=displayHistory;
-    return;
+    historyElem.innerHTML = displayHistory;
+    return ;
 };
 
 
